680743346b2724aa952c6e0cabfcac8072c58142
mortein43 <mortein43@gmail.com>
2025-08-18 11:16:36 +0300

    Some general settings and removal of unnecessary items

--- anime-frontend/src/app/page.tsx
+++ anime-frontend/src/app/page.tsx
@@ -1,36 +1,9 @@
 "use client";
-import { useEffect, useState } from "react";
-
-interface Genre {
-  id: string;
-  name: string;
-  description: string;
-}
 
 export default function Home() {
-  const [genres, setGenres] = useState<Genre[]>([]);
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/v1/genres`)
-      .then((res) => res.json())
-      .then((data) => {
-        setGenres(data.data);
-      })
-      .catch((err) => {
-        console.log("Fetch error:", err);
-      });
-  }, []);
-
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div>
       <h1>Hello World!</h1>
-      <div>
-        <ul>
-          {genres.map((genre) => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
